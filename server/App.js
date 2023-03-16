0bac48d4ba8f95550819d0083bd5d1d60bd9b74c
Aditya Ranjan <aditya74810@gmail.com>
2023-03-16 18:32:08 +0530

    Initial Commit

---
+++ server/App.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+const morgan = require('morgan');
+const cors = require('cors');
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/videoServer', {
+    // useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.Promise = global.Promise;
+
+app.use(morgan('dev'));
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+
+app.use('/api/signUp', require('./router/signUp'));
+app.use('/api/signIn',require('./router/signIn'))
+module.exports = app;
---
+++ server/server.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const app = require('./App')
+
+const config = require('./configs/default')
+const port = config.port;
+
+const server = http.createServer(app);
+server.listen(port);
+
+console.log('server is runing at ' + port)
+
